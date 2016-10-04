77f2ff41918fc7763d7a15c1886ebddb848f05a8
KamilGrzechulski <kamil.grzechulskii@gmail.com>
2016-10-04 19:15:58 +0200

    add bootstrap to project

--- src/KamilG/SiteBundle/Controller/DefaultController.php
+++ src/KamilG/SiteBundle/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace KamilG\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
