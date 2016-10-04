19571dee0b134ba71ef1c1e09730adf613c3e7f5
KamilGrzechulski <kamil.grzechulskii@gmail.com>
2016-10-04 22:00:25 +0200

    Add: Twig, Add List function etc

--- src/KamilG/SiteBundle/Controller/DefaultController.php
+++ src/KamilG/SiteBundle/Controller/DefaultController.php
@@ -3,7 +3,10 @@
 namespace KamilG\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use KamilG\SiteBundle\Form\TodoType;
+use KamilG\SiteBundle\Entity\Todo;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -17,5 +20,31 @@ class DefaultController extends Controller
         return $this->render('KamilGSiteBundle:Default:index.html.twig',
         array('lists' => $list
         ));
+    }
+
+    public function addAction(Request $request)
+    {
+        $lists = new Todo();
+        $form = $this->createForm(TodoType::class, $lists);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $save = $this->getDoctrine()->getManager();
+            $save->persist($lists);
+            $save->flush();
+
+            return $this->redirectToRoute('kamil_g_site_sucess');
+        }
+
+        return $this->render(
+            'KamilGSiteBundle:Default:add.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+    public function sucessAction()
+    {
+        return new Response("Sucess!");
     }
 }
---
+++ src/KamilG/SiteBundle/Form/TodoType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace KamilG\SiteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
+class TodoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array(
+                'label'  => 'Name',
+                'attr'   =>  array(
+                    'class'   => 'form-control')))
+
+            ->add('description', 'textarea', array(
+                'label'  => 'Description',
+                'attr'   =>  array(
+                    'class'   => 'form-control')));
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'KamilG\SiteBundle\Entity\Todo'
+        ));
+    }
+}
